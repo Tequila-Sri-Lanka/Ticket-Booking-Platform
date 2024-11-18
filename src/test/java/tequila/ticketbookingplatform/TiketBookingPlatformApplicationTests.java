4f7c21669ac70a1ced8e9c7446f4e1a3a40d2978
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-11-18 16:59:57 +0530

    first commit

---
+++ src/test/java/tequila/ticketbookingplatform/TiketBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package tequila.ticketbookingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketBookingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
